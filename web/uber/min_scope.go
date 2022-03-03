26bbafc5615a94ff28903ee56de0230e80545bc1
Young-Mac Pro <young.zhu.2012@gmail.com>
2022-03-03 21:32:51 +0800

    尽量缩小变量的作用范围

---
+++ web/uber/min_scope.go
@@ -0,0 +1,21 @@
+package uber
+
+import "io/ioutil"
+
+// 尽量缩小变量的作用范围
+
+func badScope() error {
+	err := ioutil.WriteFile("", []byte("bad"), 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func goodScope() error {
+	// err 只作用在 if 范围内
+	if err := ioutil.WriteFile("", []byte("bad"), 0644); err != nil {
+		return err
+	}
+	return nil
+}
