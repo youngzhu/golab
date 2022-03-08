fde43bdef4ea0c0a2cf08699ca77279cb5c3d027
young-win <young.zhu.2012@gmail.com>
2022-03-08 08:59:53 +0800

    SOLID

--- web/dave/solid/solid.go
+++ web/dave/solid/solid.go
@@ -1,6 +1,7 @@
 package solid
 
-// https://learnku.com/go/wikis/38440
+// 原文：https://dave.cheney.net/2016/08/20/solid-go-design
+// 译文：https://learnku.com/go/wikis/38440
 
 // SOLID
 // Single Responsibility Principle: 单一职责原则
@@ -15,3 +16,6 @@ package solid
 // Clients should not be forced to depend on methods they do not use. -- Robert C. Martin
 
 // Dependency Inversion Principle: 依赖倒置原则
+// High-level modules should not depend on low-level modules. Both should
+// depend on abstractions. Abstractions should not depend on details.
+// Details should depend on abstractions. -- Robert C. Martin
