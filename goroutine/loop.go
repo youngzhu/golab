23421d5a8e86aa47b3eb41c8e353239a35331c11
young-win <young.zhu.2012@gmail.com>
2022-03-23 09:19:35 +0800

    goroutine loop

---
+++ goroutine/loop.go
@@ -0,0 +1,20 @@
+package main
+
+import "sync"
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(x int) {
+			sendRPC(x)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	println("done")
+}
+
+func sendRPC(i int) {
+	println(i)
+}
