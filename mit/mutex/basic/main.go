fac389d4e547c6e849c50839116409c7eaf1f3ad
young-win <young.zhu.2012@gmail.com>
2022-03-24 11:15:25 +0800

    mutex basic

--- mit/mutex/basic/main.go
+++ mit/mutex/basic/main.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"sync"
-	"time"
 )
 
 func main() {
 	counter := 0
 	var mu sync.Mutex
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 10000000; i++ {
 		go func() {
 			mu.Lock()
-			defer mu.Unlock()
+			//defer mu.Unlock()
 			counter = counter + 1
+			mu.Unlock()
 		}()
 	}
-	time.Sleep(1 * time.Second)
+	//time.Sleep(1 * time.Second)
+	// 这里应该不需要锁啊。不知道示例中为什么有
+	// 用大数测试，有没有锁都是正确的
+	// 但没有sleep，即使有锁，打印的值也是错误的。defer或另一种方式都是错
+	mu.Lock()
 	println(counter)
+	mu.Unlock()
 }
