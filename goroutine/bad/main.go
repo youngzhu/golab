3bcc9e47c13d4056733130fcade2fa44c7c80cb5
young-win <young.zhu.2012@gmail.com>
2022-03-24 09:01:48 +0800

    goroutine bad loop

---
+++ goroutine/bad/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "sync"
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func() {
+			sendRPC(i)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	println("done")
+}
+
+func sendRPC(i int) {
+	println(i)
+}
