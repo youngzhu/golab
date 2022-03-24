5b4cc4f99725121fec957648682bc8940a77bb2a
young-win <young.zhu.2012@gmail.com>
2022-03-24 11:09:52 +0800

    mutex basic

---
+++ mit/mutex/basic/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+func main() {
+	counter := 0
+	var mu sync.Mutex
+	for i := 0; i < 1000; i++ {
+		go func() {
+			mu.Lock()
+			defer mu.Unlock()
+			counter = counter + 1
+		}()
+	}
+	time.Sleep(1 * time.Second)
+	println(counter)
+}
