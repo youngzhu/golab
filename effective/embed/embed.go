60e6669171598148ca2e3ea75509083edc337c07
young-win <young.zhu.2012@gmail.com>
2022-04-27 09:45:00 +0800

    Effective Go: embedding

---
+++ effective/embed/embed.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+type Job struct {
+	Command string
+	*log.Logger
+}
+
+func NewJob(command string, logger *log.Logger) *Job {
+	return &Job{command, logger}
+}
+
+// 可以覆盖原方法
+func (job *Job) Printf(format string, args ...interface{}) {
+	// 如果属性没有赋值，可以通过类型名来调用(job.Logger)
+	// 注意最后的...
+	job.Logger.Printf("%q: %s", job.Command, fmt.Sprintf(format, args...))
+}
+
+var loggerStderr = log.New(os.Stderr, "Job: ", log.Ldate)
+
+func main() {
+	job := NewJob("listen", loggerStderr)
+	job.Println("starting now...")
+
+	job.Printf("%s %d", "counter", 2022)
+}
