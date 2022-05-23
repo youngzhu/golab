ce990a3d4780aa32aa43471ce70e95e65a965bf3
young-win <young.zhu.2012@gmail.com>
2022-05-23 14:46:06 +0800

    database-access: Query for multiple rows: db.Query()

--- db-access/db-access.go
+++ db-access/db-access.go
@@ -11,6 +11,39 @@ import (
 
 var db *sql.DB
 
+type Album struct {
+	ID     int
+	Title  string
+	Artist string
+	Price  float32
+}
+
+func albumsByArtist(name string) ([]Album, error) {
+	var albums []Album
+	sqlStr := "select * from album where artist=?"
+	rows, err := db.Query(sqlStr, name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+
+	if albums == nil {
+		return nil, fmt.Errorf("albumsByArtist %q: Not found", name)
+	}
+
+	return albums, nil
+}
 func main() {
 	cfg := mysql.Config{
 		User:   "root",
@@ -32,4 +65,12 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("Connected!")
+
+	artist := "Jackson"
+	artist = "John Coltrane"
+	albums, err := albumsByArtist(artist)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
 }
