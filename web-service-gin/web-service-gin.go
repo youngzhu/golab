d0b1dbabab06ab42985bc138d78487a27d8ac585
young-win <young.zhu.2012@gmail.com>
2022-05-24 15:46:33 +0800

    web-service-gin

--- web-service-gin/web-service-gin.go
+++ web-service-gin/web-service-gin.go
@@ -4,7 +4,9 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"strconv"
 )
 
 // 加了这个所谓的标签，可以指定json串中的字段名
@@ -19,16 +21,41 @@ type album struct {
 var albums = []album{
 	{ID: 1, Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: 2, Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: 1, Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: 3, Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func addAlbums(c *gin.Context) {
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		log.Fatal(err)
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+	idNum, _ := strconv.Atoi(id)
+	for _, a := range albums {
+		if a.ID == idNum {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", addAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 
 	router.Run("localhost:8080")
 }
