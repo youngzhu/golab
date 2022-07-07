6d38dc8a650ef2bdd87c0dd9362708f96d27793e
young-win <young.zhu.2012@gmail.com>
2022-07-07 14:57:04 +0800

    default 防止永久性阻塞，并不会一直执行default

--- foundation/iselect/s2/s2.go
+++ foundation/iselect/s2/s2.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"log"
+	"time"
+)
 
 // default 防止永久性阻塞
 
@@ -11,18 +14,29 @@ func process(in chan string) {
 	}
 }
 
+func process2(in chan string) {
+	for {
+		time.Sleep(5 * time.Second)
+		in <- "input2"
+	}
+}
+
 func main() {
 	in := make(chan string)
+	in2 := make(chan string)
 
 	go process(in)
+	go process2(in2)
 
 	for {
 		time.Sleep(2 * time.Second)
 		select {
 		case s := <-in:
-			println("output:", s)
+			log.Println("output:", s)
+		case s2 := <-in2:
+			log.Println("output:", s2)
 		default:
-			println("wait...")
+			log.Println("wait...")
 		}
 	}
 }
